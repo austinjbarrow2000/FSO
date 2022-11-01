29cc15906135e40901ef0b6ad5b627a0fc3e939c
Austin Barrow <austinjbarrow2000@gmail.com>
2022-11-01 01:18:41 -0400

    Add multi course functionality

--- part2/course-info/src/Components/Course.js
+++ part2/course-info/src/Components/Course.js
@@ -22,7 +22,7 @@ const Part = (props) => {
 
 const Total = ({parts}) => {
     return (
-        <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0 )}</p>
+        <h3>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0 )} </h3>
     )
 }
 
