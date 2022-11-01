0bf0def35ead0301f30b93bfd1af999e8f3af2a9
Austin Barrow <austinjbarrow2000@gmail.com>
2022-11-01 02:01:29 -0400

    Add add person functionality and alert duplicates

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,45 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas'}
   ]) 
   const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if(persons.filter(person => person.name === newName).length === 0) {
+      setPersons(persons.concat({name: newName}))
+      setNewName('')
+    } else {
+      alert(`${newName} is already in the phonebook`)
+    }
+    
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit = {addPerson}>
         <div>
-          name: <input />
+          name: 
+          <input 
+            value = {newName}
+            onChange = {handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      
+      {persons.map(person =>
+        <p key = {person.name}>{person.name}</p>
+      )}
     </div>
   )
 }
